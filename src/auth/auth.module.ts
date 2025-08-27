8bb509591c8ede0fce001e8c26a5fc999d037743
Boris Ushakov <inlvert@gmail.com>
2025-08-27 23:32:17 +0300

    add change to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { RefreshToken, RefreshTokenSchema } from './model/refreshToken.schema';
 import { User, UserSchema } from 'src/user/schema/user.schema';
 import { JwtModule } from '@nestjs/jwt';
+import { SessionService } from 'src/services/session.service';
+import { JwtTokenService } from 'src/services/jwt-token.service';
 
 @Module({
   imports: [
@@ -15,6 +17,6 @@ import { JwtModule } from '@nestjs/jwt';
     JwtModule.register({}),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, SessionService, JwtTokenService],
 })
 export class AuthModule {}
