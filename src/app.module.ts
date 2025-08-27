d84b484ddda8761a7c7b2750d9a6d09fb8f2cfbb
Boris Ushakov <inlvert@gmail.com>
2025-08-27 23:04:36 +0300

    add change to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,16 @@ import { ProductsService } from './products/products.service';
 import { ProductModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CONSTANTS } from './config/constants';
-
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductModule, MongooseModule.forRoot(CONSTANTS.CONNECT_TO_DB!)],
+  imports: [
+    MongooseModule.forRoot(CONSTANTS.CONNECT_TO_DB!),
+    ProductModule,
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
