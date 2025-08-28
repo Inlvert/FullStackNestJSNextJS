deea23f2fe47b2c0b4fab1973c499929047b3c19
Boris Ushakov <inlvert@gmail.com>
2025-08-28 13:11:18 +0300

    add check router 'users' get all

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,23 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './schema/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
+import { JwtTokenGuard } from 'src/guards/jwt-token.guard';
+import { Request } from 'express';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @UseGuards(JwtTokenGuard)
   @Get()
-  getAll(): Promise<User[]> {
+  getAll(@Req() req: Request): Promise<User[]> {
+    console.log((req as any).user);
     return this.userService.getAllUsers();
   }
 
   @Post()
-  create(@Body() cretaUserDto: CreateUserDto): Promise<User> {
-    return this.userService.creatUser(cretaUserDto);
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.userService.creatUser(createUserDto);
   }
 }
