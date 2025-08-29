8713178a57e453cd2bed42c812639e9a7839346d
Boris Ushakov <inlvert@gmail.com>
2025-08-29 17:04:02 +0300

    add new name JwtAccessTokenGuard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,14 +2,14 @@ import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './schema/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
-import { JwtTokenGuard } from 'src/guards/jwt-token.guard';
+import { JwtAccessTokenGuard } from 'src/guards/jwt-Accesstoken.guard';
 import { Request } from 'express';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @UseGuards(JwtTokenGuard)
+  @UseGuards(JwtAccessTokenGuard)
   @Get()
   getAll(@Req() req: Request): Promise<User[]> {
     console.log((req as any).user);
