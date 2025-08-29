ee836e83e4f3a69ca54856844f4e71398684c8d9
Boris Ushakov <inlvert@gmail.com>
2025-08-29 17:03:14 +0300

    add refreshSession

--- src/services/session.service.ts
+++ src/services/session.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { JwtTokenService } from './jwt-token.service';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from 'src/user/schema/user.schema';
@@ -7,11 +7,12 @@ import {
   refreshTokenDocument,
 } from 'src/auth/model/refreshToken.schema';
 import { Model } from 'mongoose';
+import { AuthResponse } from 'src/auth/dto/auth-response.dto';
 
 @Injectable()
 export class SessionService {
   constructor(
-    // @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
     @InjectModel(RefreshToken.name)
     private refreshTokenModel: Model<refreshTokenDocument>,
     private readonly jwtTokenService: JwtTokenService,
@@ -30,6 +31,34 @@ export class SessionService {
       token: tokenPair.refreshToken,
       userId: user._id,
     });
+
+    return {
+      user,
+      tokenPair,
+    };
+  }
+
+  async refreshSession(refreshTokenInstants): Promise<AuthResponse> {
+    const user = await this.userModel.findById(refreshTokenInstants.userId);
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    const tokenPayload = {
+      id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    };
+
+    const tokenPair = await this.jwtTokenService.createTokenPair(tokenPayload);
+
+    await this.refreshTokenModel.findOneAndUpdate(
+      { token: refreshTokenInstants.token },
+      { token: tokenPair.refreshToken },
+    );
+
+    // console.log(tokenPair)
 
     return {
       user,
