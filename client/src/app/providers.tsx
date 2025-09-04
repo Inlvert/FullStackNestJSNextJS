450e73ee1b5d180006c3d6fc6a077c8bbb589406
Boris Ushakov <inlvert@gmail.com>
2025-09-04 21:52:23 +0300

    add providers

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,10 +1,12 @@
-"use client"
-import {HeroUIProvider} from '@heroui/react'
+"use client";
+import { store } from "@/redux/store";
+import { HeroUIProvider } from "@heroui/react";
+import { Provider } from "react-redux";
 
-export function Providers({children}: { children: React.ReactNode }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <HeroUIProvider>
-      {children}
-    </HeroUIProvider>
-  )
+    <Provider store={store}>
+      <HeroUIProvider>{children}</HeroUIProvider>
+    </Provider>
+  );
 }
