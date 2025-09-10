de74e231889b5775fd18e2128409aeba49bdfd14
Boris Ushakov <inlvert@gmail.com>
2025-09-10 13:24:41 +0300

    add hook useSocket

---
+++ client/src/hooks/useSoket.ts
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from "react";
+import { io, Socket } from "socket.io-client";
+
+export function useSocket() {
+  const socketRef = useRef<Socket | null>(null);
+
+  useEffect(() => {
+    socketRef.current = io("http://localhost:5000", {
+      transports: ["websocket"],
+    });
+
+    socketRef.current.on("connect", () => {
+      console.log("connected:", socketRef.current?.id);
+    });
+
+    socketRef.current.on("disconnected", () => {
+      console.log("disconnected");
+    });
+
+    return () => {
+      socketRef.current?.disconnect();
+    };
+  }, []);
+
+  return socketRef.current;
+}
