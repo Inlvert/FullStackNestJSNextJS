023f0cf6bc9ab742c8a05f786e7af807e4ba7406
Boris Ushakov <inlvert@gmail.com>
2025-09-30 14:58:23 +0300

    add 6

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,5 +1,5 @@
 const CONSTANTS = {
-  baseURL: "https://full-stack-nest-js-next-qwxn0bls4-inlverts-projects.vercel.app",
+  baseURL: "https://full-stack-nest-js-next-j5wwteme2-inlverts-projects.vercel.app",
   REFRESH_TOKEN: "refreshToken"
 };
 
