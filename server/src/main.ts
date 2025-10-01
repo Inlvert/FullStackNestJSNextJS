db6897087125c42445ff54e71811ccc2fa927355
Boris Ushakov <inlvert@gmail.com>
2025-10-01 12:36:45 +0300

    add 15

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,12 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://personal-recipe-discovery-platform-two.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
-  await app.listen(process.env.PORT ?? 5000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
